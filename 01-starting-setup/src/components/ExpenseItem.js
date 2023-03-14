bbdf5114ddc46bde2a63eb3ed822832e09a3f5a9
Afis KOUSSE <mahoutinafis@gmail.com>
2023-03-14 12:10:32 +0100

    Style component

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className='expense-item'>
       <div>March 28th 2023</div>
-      <div>
+      <div className='expense-item__description'>
         <h2>Car Insurance</h2>
-        <div>$2506</div>
+        <div className='expense-item__price'>$2506</div>
       </div>
     </div>
   );
