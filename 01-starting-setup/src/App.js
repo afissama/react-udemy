eb2b82524fc8cf18b80d369dbcbcc33fd94dfd48
Afis KOUSSE <mahoutinafis@gmail.com>
2023-03-14 11:30:00 +0100

    first component

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,8 +1,17 @@
+import ExpenseItem from './components/ExpenseItem'
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
+  );
+}
+
+function Hello(){
+  return(
+    <p>This is Hello world !!</p>
   );
 }
 
